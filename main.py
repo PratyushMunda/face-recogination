fac93686e518c40ef0da28fe7f5919a518d454a0
Pratyush Munda <150261697+PratyushMunda@users.noreply.github.com>
2024-01-17 16:34:24 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,42 @@
+import cv2
+import threading
+from DeepFace import DeepFace
+
+cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,480)
+
+def check_face(frame):
+    global face_match
+    try:
+        if DeepFace.verify(frame,refrence_img.copy())["Verified"]:
+            face_match = True
+        else:
+            face_match = False    
+
+    except ValueError:
+        face_match = False
+
+while True:
+    ret, frame = cap.read()
+
+    if ret:
+        if counter % 30 == 0:
+            try:
+                threading.Thread(target=check_face,args=(frame.copy(),) ),start()
+            except ValueError:
+                pass    
+        counter += 1   
+
+        if face_match:
+            cv2.putText(frame,"MATCH",(20,450),cv2.FONT_HERWHEY_SIMPLEX, 2, (0,255,0),3) 
+        else:
+            cv2.putText(frame,"NO MATCH",(20,450),cv2.FONT_HERWHEY_SIMPLEX, 2, (0,255,0),3)     
+        cv2.imshow("Video",frame)
+            
+    key = cv2.waitkey(1)
+    if key == ord("q"):
+        break
+
+cv2.destroyAllWindows()
